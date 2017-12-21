fb4798f24760f3b8f9b94d89f7b24224cddeee59
Konstantin Pankov <kspzel@gmail.com>
2017-12-21 03:23:58 +0300

    Add parser

--- src/org/ksplabs/sveet/SystemVerilogLanguage.java
+++ src/org/ksplabs/sveet/SystemVerilogLanguage.java
@@ -3,7 +3,9 @@ package org.ksplabs.sveet;
 import org.netbeans.api.lexer.Language;
 import org.netbeans.modules.csl.spi.DefaultLanguageConfig;
 import org.netbeans.modules.csl.spi.LanguageRegistration;
+import org.netbeans.modules.parsing.spi.Parser;
 import org.ksplabs.sveet.lexer.SVTokenId;
+import org.ksplabs.sveet.parser.SVParser;
 
 @LanguageRegistration(mimeType = "text/x-systemverilog")
 public class SystemVerilogLanguage extends DefaultLanguageConfig {
@@ -17,5 +19,10 @@ public class SystemVerilogLanguage extends DefaultLanguageConfig {
     public String getDisplayName() {
         return "System Verilog";
     }
+    
+//    @Override
+//    public Parser getParser() {
+//        return new SVParser();
+//    }
 
 }
--- src/org/ksplabs/sveet/VerilogLanguage.java
+++ src/org/ksplabs/sveet/VerilogLanguage.java
@@ -3,7 +3,9 @@ package org.ksplabs.sveet;
 import org.netbeans.api.lexer.Language;
 import org.netbeans.modules.csl.spi.DefaultLanguageConfig;
 import org.netbeans.modules.csl.spi.LanguageRegistration;
+import org.netbeans.modules.parsing.spi.Parser;
 import org.ksplabs.sveet.lexer.SVTokenId;
+import org.ksplabs.sveet.parser.SVParser;
 
 @LanguageRegistration(mimeType = "text/x-verilog")
 public class VerilogLanguage extends DefaultLanguageConfig {
@@ -16,6 +18,11 @@ public class VerilogLanguage extends DefaultLanguageConfig {
     @Override
     public String getDisplayName() {
         return "Verilog";
+    }
+
+    @Override
+    public Parser getParser() {
+        return new SVParser();
     }
 
 }
